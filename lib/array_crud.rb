1877d119f3e18603856771bfa86d0a4557f50d8c
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-05 18:57:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,9 @@ end
 
 def add_element_to_start_of_array(array, element)
 
+  array = ["wow", "I", "am", "really", "learning"]
 
+  array.unshift ("yay!")
 end
 
 def remove_element_from_end_of_array(array)
